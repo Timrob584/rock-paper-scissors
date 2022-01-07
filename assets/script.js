f245cfc98126cd6db6c5fa0543d236fc061bfdb6
Timrob584 <timrob584@gmail.com>
2022-01-07 10:45:29 -0500

    added Modal functionality

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,17 @@
-const modalEl = document.getElementById("modal");
+const modal = document.getElementById("rulesModal");
+const modalBtn = document.getElementById("modalBtn");
+const closeModal = document.getElementById("close");
+const paper = document.getElementById("paper");
+const rock = document.getElementById("rock");
+const scissors = document.getElementById("scissors");
+
+
+// Open Modal When User Clicks Button
+modalBtn.onclick = function () {
+    modal.style.display = "block";
+};
+
+// Close the Modal When User Clicks the Close Button
+closeModal.onclick = function () {
+    modal.style.display = "none";
+};
